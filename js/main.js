5cb13da9801d9ba2e1369081203d72096375f062
Revan-Darth <nitro.Knight3080@gmail.com>
2022-08-14 01:04:39 +0700

    added curtains and adaptive

--- js/main.js
+++ js/main.js
@@ -7,3 +7,45 @@ if(menuBurger) {
 		menuBody.classList.toggle('active');
 	});
 }
+// Определение тачСкрина
+const isMobile = {
+	Android: function() {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function() {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function() {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function() {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function() {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+// Добавление класса на body в зависимости от экрана
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+
+	let menuArrows = document.querySelectorAll('.menu__arrow'); // Тут мы ищем стрелочку у подменю и если она есть, то при клике на неё добавляем класс _active
+	if (menuArrows.length > 0 ) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.parentElement.classList.toggle('.active')
+			});
+		}
+	}
+} else {
+	document.body.classList.add('_pc'); // Если не тачСкрин то будет класс пк
+}
